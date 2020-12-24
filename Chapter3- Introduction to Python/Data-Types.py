6c898bfa6d82e61bf14b3db9bc959e06eb75fae8
asultan <ahmed.e.sultan@gmail.com>
2020-12-24 19:13:54 +0200

    Codes for Chapter 3 in DevNet Associate Book (200-901)

---
+++ Chapter3- Introduction to Python/Data-Types.py
@@ -0,0 +1,73 @@
+# # # One Line Comment
+# # # number1 = 10
+# # # number2 = 20
+# # # number3 = 30
+# # # print(number3)
+# # #
+# # # names = ["ahmed", "ali"]
+# # #
+# # # sunrise = True
+# # #
+# # # course = "devnet"
+# #
+# # # names = ("ahmed", "mohammed", "ebrahim", "ahmed")
+# #
+# # # employees = {"ahmed": 31, "mohammed": 22, "ali": 20}
+# #
+# # devices = {
+# #     "Sw1": "192.168.10.10",
+# #     "Sw2": "192.168.10.20",
+# #     "Sw3": "192.168.10.30",
+# # }
+# # print(devices["Sw3"])
+#
+# device_type = input("Enter your device type 'cisco' or 'juniper': ")
+#
+# if device_type == "cisco":
+#     print("You choosed cisco")
+#     print("You choosed cisco")
+#     print("You choosed cisco")
+#     print("You choosed cisco")
+#     print("You choosed cisco")
+#     print("You choosed cisco")
+#     print("You choosed cisco")
+#     print("You choosed cisco")
+#     print("You choosed cisco")
+#     print("You choosed cisco")
+#
+# elif device_type == "juniper":
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#     print("You choosed juniper")
+#
+# else:
+#     print("You choosed wrong device !")
+#
+# number1 = int(input("Enter your first number: "))
+# number2 = int(input("Enter your second number: "))
+#
+# print("Addition = ", number1 + number2)
+# print("Sub = ", number1 - number2)
+# print("Multi = ", number1 * number2)
+# print("Div = ", number1 / number2)
+# print("Sum", number1 % number2)
+
+# name = ["ahmed", "ali"]
+# print(name[0])
+# print(name[1])
+#
+
+
+for i in range(100000):
+    print("Ahmed:", i)
+    i += 1
