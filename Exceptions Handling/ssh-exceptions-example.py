a143cd7886cd4e447033c30f1592a0a0e413e4b4
asultan <ahmed.e.sultan@gmail.com>
2021-01-12 14:25:33 +0200

    add new directory

--- Exceptions Handling/ssh-exceptions-example.py
+++ Exceptions Handling/ssh-exceptions-example.py
@@ -8,8 +8,8 @@ from netmiko.ssh_exception import SSHException  # SSH is not enabled Exception
 # Define all switches IPs in a List
 cisco_switches = ["192.168.43.10", "192.168.43.11", "192.168.43.12", "192.168.43.13"]
 
-try:
-    for ip in cisco_switches:
+for ip in cisco_switches:
+    try:
         ssh_info = {
             "device_type": "cisco_ios",
             "host": ip.strip(),
@@ -21,19 +21,18 @@ try:
         output = net_connect.send_command("show runn | i hostname")
         print(output)
 
+    # Raise this Exception if SSH Username or Password is wrong
+    except AuthenticationException:
+        print("Authentication Failure:", ssh_info["host"])
 
-# Raise this Exception if SSH Username or Password is wrong
-except AuthenticationException:
-    print("Authentication Failure:", ssh_info["host"])
+    # Raise this Exception if Device is down or the link is down
+    except NetMikoTimeoutException:
+        print("Timeout to device:", ssh_info["host"], ", Check the host IP and Make sure it's Up and Running")
 
-# Raise this Exception if Device is down or the link is down
-except NetMikoTimeoutException:
-    print("Timeout to device:", ssh_info["host"], ", Check the host IP and Make sure it's Up and Running")
+    # Raise this Exception if SSH is not enabled
+    except SSHException:
+        print("SSH Issue, Are you sure SSH is enabled?", ssh_info["host"])
 
-# Raise this Exception if SSH is not enabled
-except SSHException:
-    print("SSH Issue, Are you sure SSH is enabled?", ssh_info["host"])
-
-# Raise this Exception for anything else
-except Exception as unkown_error:
-    print("Some other error:"), unkown_error
+    # Raise this Exception for anything else
+    except Exception as unkown_error:
+        print("Some other error:"), unkown_error
