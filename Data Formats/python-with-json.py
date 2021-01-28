67e8847a69f832795eea72ba059bfe102058f6ef
asultan <ahmed.e.sultan@gmail.com>
2021-01-28 16:49:20 +0200

    test

--- Data Formats/python-with-json.py
+++ Data Formats/python-with-json.py
@@ -4,13 +4,11 @@ import json
 with open("Example1.json") as f:
     data = f.read()
 
-# json_dict is a dictionary, and json.loads takes care of
-# placing our JSON data into it.
+# json_dict is a dictionary, and json.loads takes care of placing our JSON data into it.
 json_dict = json.loads(data)
 
-
 # Print the whole json dictionary
-print(json_dict)
+print(json_dict, '\n')
 
 for k, v in json_dict.items():
     print("-- The key '{0}' has value of '{1}'.".format(str(k), str(v)))
