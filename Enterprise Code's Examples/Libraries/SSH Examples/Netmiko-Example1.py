d9ece86b3ba1a571f0be69b4009ab2641fdfa48b
asultan <ahmed.e.sultan@gmail.com>
2021-02-07 19:37:18 +0200

    add new directory

--- Enterprise Code's Examples/Libraries/SSH Examples/Netmiko-Example1.py
+++ Enterprise Code's Examples/Libraries/SSH Examples/Netmiko-Example1.py
@@ -1,10 +1,12 @@
+""" Send configuration to device using Netmiko """
+
 # Call Connect Handler function from Netmiko Library
 
 from netmiko import ConnectHandler
 
 connection = {
     "device_type": "cisco_ios",
-    "host": "192.168.43.12",
+    "host": "192.168.43.10",
     "username": "admin",
     "password": "cisco",
     "secret": "cisco"
@@ -12,7 +14,7 @@ connection = {
 ssh_connect = ConnectHandler(**connection)  # Establish SSH session  with HOST
 
 if ssh_connect:  # Check if the connection established !
-    print("Success !")
+    print("Connection Succeeded !\n")
 
-output = ssh_connect.send_command("show runn | i hostname")
+output = ssh_connect.send_config_set("hostname SWx")
 print(output)
